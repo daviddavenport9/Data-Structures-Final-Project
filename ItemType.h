0427b2d1d8fd1b6cbb93b01e84066b0128220c50
daviddavenport9 <45472470+daviddavenport9@users.noreply.github.com>
2020-02-14 12:13:14 -0500

    Create ItemType.h

---
+++ ItemType.h
@@ -0,0 +1,19 @@
+#ifndef ITEMTYPE_H
+#define ITEMTYPE_H
+
+enum Comparison {LESS, EQUAL, GREATER };
+
+class ItemType{
+
+public : 
+  ItemType();
+  Comparison compareTo(ItemType) const;
+  int getValue() const;
+  void initialize(int number);
+  void print();
+  
+  
+private:
+  int value;
+};
+#endif
